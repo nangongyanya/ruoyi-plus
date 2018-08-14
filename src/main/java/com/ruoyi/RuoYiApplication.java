e4a45ba81bb6b36e621e2c9b17574423da0779e1
LiMing <346525951@qq.com>
2018-08-14 21:56:50 +0800

    application修改

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * 启动程序
  * 
- * @author ruoyi
+ * @author Rimon
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @MapperScan("com.ruoyi.project.*.*.mapper")
--- src/main/java/com/ruoyi/RuoYiServletInitializer.java
+++ src/main/java/com/ruoyi/RuoYiServletInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 /**
  * web容器中进行部署
  * 
- * @author ruoyi
+ * @author Rimon
  */
 public class RuoYiServletInitializer extends SpringBootServletInitializer
 {
